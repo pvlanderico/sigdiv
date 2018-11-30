1f9344e0b489117e9a94305ea04b943f422e9bfe
Julian Gutierrez <juliansouza@gmail.com>
2018-11-30 12:35:35 -0200

    Fixed links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :creditors
   resources :debts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'debts#index'
 end
