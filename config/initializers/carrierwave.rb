2ee62783e7fc4f71616df678738452f5f59d4b20
Julian Gutierrez <juliansouza@gmail.com>
2018-12-19 14:47:29 -0200

    Fixed settings files

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
-  config.ftp_host = 'xxx'
-  config.ftp_port = 'xxx'
-  config.ftp_user = 'xxx'
-  config.ftp_passwd = 'xxx'
-  config.ftp_folder = 'xxx'
-  config.ftp_url = 'xxx'
+  config.ftp_host = Sadp::Application.config.ftp.host
+  config.ftp_port = Sadp::Application.config.ftp.port
+  config.ftp_user = Sadp::Application.config.ftp.username
+  config.ftp_passwd = Sadp::Application.config.ftp.password
+  config.ftp_folder = Sadp::Application.config.ftp.folder
+  config.ftp_url = Sadp::Application.config.ftp.url
   config.ftp_passive = true # false by default
   config.ftp_tls = false # false by default
   config.ftp_chmod = false
