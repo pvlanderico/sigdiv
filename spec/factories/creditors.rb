a3be5dc527ef5fb1d1a0b6270682fa3c773547e2
Julian Gutierrez <juliansouza@gmail.com>
2019-02-08 15:11:40 -0200

    Added spec

--- spec/factories/creditors.rb
+++ spec/factories/creditors.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :creditor do
+
+    name { Faker::Bank.name }
+    financial_agent { false }
     
+    trait :financial_agent do
+    	financial_agent { true }
+    end
+
+    factory :financial_agent, traits: [:financial_agent]
   end
 end
--- spec/models/creditor_spec.rb
+++ spec/models/creditor_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Creditor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Creditor, type: :model do
+	
+	before do
+  	create(:creditor)
+  	create(:financial_agent)
+  end
+	
+  describe '.financial_agents' do 
+  	
+   	it 'has just one bank' do
+  		expect(Creditor.financial_agents.count).to eq(1) 
+  	end
+  end
+
 end
