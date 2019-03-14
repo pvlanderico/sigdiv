d4a07bda26217e609fe3debbd79fb8ffd032e3a2
Julian Gutierrez <juliansouza@gmail.com>
2019-03-14 12:20:54 -0300

    Added function

--- app/models/transaction/payment.rb
+++ app/models/transaction/payment.rb
@@ -1,5 +1,6 @@
 class Payment < Transaction
 	# before_save :set_interest
+	# before_save :set_principal
 
 	def final_outstanding_balance
 		start_outstanding_balance - value
@@ -8,5 +9,9 @@ class Payment < Transaction
 	private
 		def set_interest
 			interest = debt.interest
+		end
+
+		def set_principal
+			principal = debt.next_installment - interest
 		end
 end
