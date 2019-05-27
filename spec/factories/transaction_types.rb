2e65ead82d36443b87552a3bbd9f2b018c89a688
Julian Gutierrez <juliansouza@gmail.com>
2019-05-27 14:14:32 -0300

    Adicionadas factories

--- spec/factories/transaction_types.rb
+++ spec/factories/transaction_types.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
-  factory :transaction_type do
-    name { "MyString" }
-    operator { "MyString" }
-  end
+	factory :transaction_type do
+		factory :withdraw do
+		  name { TransactionType::BASIC_TYPES[1][:name] }
+		  operator { TransactionType::BASIC_TYPES[1][:operator] }
+		end
+
+		factory :amortization do
+		  name { TransactionType::BASIC_TYPES[2][:name] }
+		  operator { TransactionType::BASIC_TYPES[2][:operator] }
+		end
+	end
 end
--- spec/models/debt_spec.rb
+++ spec/models/debt_spec.rb
@@ -2,46 +2,7 @@ require 'rails_helper'
 
 describe Debt, type: :model do
   before(:each) do  	
-  	@debt = create(:cef, charges: [ build(:adm_tax), build(:credit_risk) ])
-    
-    create(:withdraw_0515, debt: @debt)
-    create(:withdraw_0615, debt: @debt)
-    create(:withdraw_0715, debt: @debt)
-    create(:withdraw_0815, debt: @debt)
-    create(:withdraw_0915, debt: @debt)
-    create(:withdraw_1015, debt: @debt)
-    create(:withdraw_1115, debt: @debt)
-    create(:withdraw_1215, debt: @debt)
-    create(:withdraw_0316, debt: @debt)
-    create(:withdraw_0516, debt: @debt)
-    create(:withdraw_0616, debt: @debt)
-    create(:withdraw_0716, debt: @debt)
-    create(:withdraw_0816, debt: @debt)
-    create(:withdraw_0916, debt: @debt)
-    create(:withdraw_1016, debt: @debt)
-    create(:withdraw_1116, debt: @debt)
-    create(:withdraw_1216, debt: @debt)
-    create(:withdraw_0117, debt: @debt)
-    create(:withdraw_0217, debt: @debt)
-    create(:withdraw_0317, debt: @debt)
-    create(:withdraw_0617, debt: @debt)
-    create(:withdraw_0917, debt: @debt)
-    create(:withdraw_1117, debt: @debt)        
-    create(:payment_1217, debt: @debt)
-    create(:payment_0118, debt: @debt)
-    create(:withdraw_0218, debt: @debt)    
-    create(:payment_0218, debt: @debt)
-    create(:payment_0318, debt: @debt)
-    create(:payment_0418, debt: @debt)
-    create(:payment_0518, debt: @debt)
-    create(:payment_0618, debt: @debt)
-    create(:payment_0718, debt: @debt)
-    create(:payment_0818, debt: @debt)
-    create(:payment_0918, debt: @debt)
-    create(:payment_1018, debt: @debt)
-    create(:withdraw_1018, debt: @debt)
-    create(:payment_1118, debt: @debt)
-    create(:payment_1218, debt: @debt)
+  	@debt = create(:debt, transaction_infos: [create(:debt_withdraw), create(:debt_amortization)])
 
   	@debts = create_list(:debt, 30)
 	end
@@ -110,11 +71,8 @@ describe Debt, type: :model do
     it 'Returns the correct value' do
       expect(@debt.show_outstanding_balance).to eq(Decimal.new(13022707.24222250))
     end
-  end
-   
-  describe '#charges_total' do
-  	it 'charges sum is correct'
-  end
+  end   
+  
 
   describe '#status' do
   	context 'when debt is in grace period' do
