c95ff4bcb764ec3d8726b64697b5e57cd104a01c
Julian Gutierrez <juliansouza@gmail.com>
2019-06-27 14:35:30 -0300

    Consertada factory

--- spec/factories/currencies.rb
+++ spec/factories/currencies.rb
@@ -1,20 +1,6 @@
 FactoryBot.define do
-<<<<<<< HEAD
-	factory :currency do
-		
-		factory :brl do
-			name { 'BRL' }
-			formula { '1' }
-		end
-
-		factory :uprd do
-			name { 'UPRD' }
-			formula { 'TODO' }
-		end
-=======
 	factory :currency, aliases: [:uprd] do
 		name { 'UPRD' }
 		formula { '0.5' }
->>>>>>> withdraw-refactor
 	end
 end
