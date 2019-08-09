36e88d480c6c3f288d7cbe40892582e40757489c
fabianosb <fabianosb2005@gmail.com>
2019-08-09 13:55:26 -0300

    wip - desemvolvendo arquivo seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_190924) do
+ActiveRecord::Schema.define(version: 2019_08_06_193127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
