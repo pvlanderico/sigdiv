a77d72c0aa8a84a5ac90262dbf71483d319fffaf
Julian Gutierrez <juliansouza@gmail.com>
2019-09-25 12:53:32 -0300

    Criando tela de relatórios

--- app/controllers/month_reports_controller.rb
+++ app/controllers/month_reports_controller.rb
@@ -1,8 +1,10 @@
 class MonthReportsController < ApplicationController
   
   def show
-    @projection_debt = ProjectionDebt.new Debt.find params[:debt_id], params[:start_date], params [:end_date]
- 
+    start_date = Date.new(params[:year].to_i, params[:month].to_i)
+    end_date = start_date.end_of_month
+    @projection_debt = ProjectionDebt.new(Debt.find(params[:debt_id]), start_date, end_date)
+    
     render pdf: "relatorio_mensal",
      			 page_size: "A4"    
   end
