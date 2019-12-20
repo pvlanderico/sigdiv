1630a2e64f202e3574eb0c2900ff23b236d272d8
Julian Gutierrez <juliansouza@gmail.com>
2019-12-20 15:47:08 -0300

    Configurando para deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
-  config.ftp_host = Sigdiv::Application.config.ftp.host
-  config.ftp_port = Sigdiv::Application.config.ftp.port
-  config.ftp_user = Sigdiv::Application.config.ftp.username
-  config.ftp_passwd = Sigdiv::Application.config.ftp.password
-  config.ftp_folder = Sigdiv::Application.config.ftp.folder
-  config.ftp_url = Sigdiv::Application.config.ftp.url
+  config.ftp_host = nil#Sigdiv::Application.config.ftp.host
+  config.ftp_port = nil#Sigdiv::Application.config.ftp.port
+  config.ftp_user = nil#Sigdiv::Application.config.ftp.username
+  config.ftp_passwd = nil#Sigdiv::Application.config.ftp.password
+  config.ftp_folder = nil#Sigdiv::Application.config.ftp.folder
+  config.ftp_url = nil#Sigdiv::Application.config.ftp.url
   config.ftp_passive = true # false by default
   config.ftp_tls = false # false by default
   config.ftp_chmod = false
