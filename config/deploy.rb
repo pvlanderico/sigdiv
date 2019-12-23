989153f151a01f8c7dc15c699959bdca1ed9c608
Julian Gutierrez <juliansouza@gmail.com>
2019-12-23 13:01:42 -0300

    Configurando para deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,3 +41,17 @@ set :passenger_restart_with_touch, true
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+append :linked_files, "config/master.key"
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
